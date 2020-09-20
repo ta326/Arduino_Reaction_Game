c4d24a5ddb3b30e8c4fbfd385d27651f74e1847a
ta326 <ta326@cornell.edu>
2020-09-19 22:29:11 -0400

    deleted all files

--- akk72-ta326-master/akk72-ta326-master/README.md
+++
@@ -1,2 +0,0 @@
-# This is the link to our website page
-https://pages.github.coecis.cornell.edu/ece3140-sp2020/akk72-ta326/
--- akk72-ta326-master/akk72-ta326-master/uart.h
+++
@@ -1,7 +0,0 @@
-#ifndef __UART_H__
-#define __UART_H__
-void uart_init(void);
-void uart_putchar (char ch);
-void uart_put(char *ptr_str);
-
-#endif
